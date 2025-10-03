d0a9ea0ac79765053e2a75053977a9bd7ae290fb
TheLunarDev <153661521+Mayank2177@users.noreply.github.com>
2025-10-03 18:54:29 +0530

    Update vite.config.ts

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "b7e86e66-fc85-4e3b-95de-77460b9dd5d3-00-3jlvvcuq7h69q.sisko.replit.dev",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
